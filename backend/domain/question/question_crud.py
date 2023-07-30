ad3c40d8832cd6fffe0414a326f9030e6f2a10d1
idenrai <geniuskwon966@gmail.com>
2023-07-30 12:16:48 +0900

    Add getList

---
+++ backend/domain/question/question_crud.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Session
+
+from models import Question
+
+
+def get_question_list(db: Session):
+    question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    return question_list
--- backend/domain/question/question_router.py
+++ backend/domain/question/question_router.py
@@ -3,17 +3,25 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from database import get_db
-from models import Question
+from domain.question import question_crud
+from domain.question import question_schema
 
+"""
+MEMO
+FastAPI의 API 함수는 비동기 함수로도 사용할 수 있다.
+https://fastapi.tiangolo.com/async/
+"""
 # router 파일에는 APIRouter로 생성한 router 객체가 필요
 router = APIRouter(
     prefix="/api/question",
 )
 
 
+# @router.get Annotation에 response_model을 추가
+# => question_list의 리턴값이 Question 스키마로 구성된 리스트임을 의미
 # question_list 함수의 매개변수로 db: Session = Depends(get_db) 객체를 주입
-@router.get("/list")
+@router.get("/list", response_model=list[question_schema.Question])
 def question_list(db: Session = Depends(get_db)):
     # 질문 목록 조회
-    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    _question_list = question_crud.get_question_list(db)
     return _question_list
