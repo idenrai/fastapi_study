98288a4deb5b75f3dbbe79b43d95cbf8489cd48d
idenrai <geniuskwon966@gmail.com>
2023-08-07 00:48:38 +0900

    Add id

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -48,7 +48,7 @@
     <tbody>
       {#each question_list as question, i}
         <tr>
-          <td>{i + 1}</td>
+          <td>{total - $page * size - i}</td>
           <td>
             <!--
                 해시 기반 라우팅
